e562328af4194303729c2cbc701ecf4995bee56d
User <User@DESKTOP-90L3J4F.BS14.LAN>
2021-11-03 13:58:13 +0100

    höhlen

--- 1984/src/Main.java
+++ 1984/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sind wir uns sicher mit dieser Sache?");
+		System.out.println("das leben war besser, als wir noch in den höhlen waren");
 		// huhu
 	}
 
