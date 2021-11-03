4de3a486d071b9344a58f2c6b09a245eaa5a15dd
cash <info@cashmny.com>
2021-11-03 13:52:02 +0100

    s

--- 1984/src/Main.java
+++ 1984/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sind wir uns sicher mit dieser Sache?");
+		// huhu
 	}
 
 }
