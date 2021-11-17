f14b6714aa54ede86f072a65a49730b797363e7b
JimFusselmansch <fynnhasselmann@web.de>
2021-11-17 11:07:32 +0100

    smith

--- 1984/src/Character.java
+++ 1984/src/Character.java
@@ -1,4 +1,16 @@
 
 public class Character {
-	// der erste charakter
+	/* der erste charakter 
+	Winston Smtih: */
+
+			public class WinstonSmith {
+				private String 	name;
+				private String 	vorName;
+				private int 	alter;
+				private String 	krankheiten;
+				private int 	healthPoints;
+				private int 	traitorPoints;
+				private int 	partyPoints;
+
+			}
 }
