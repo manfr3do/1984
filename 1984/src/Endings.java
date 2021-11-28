20fc7a158fb0b80b164a9373a4e6387d6f66e341
cash <info@cashmny.com>
2021-11-28 13:07:47 +0100

    added system exit for endings

--- 1984/src/Endings.java
+++ 1984/src/Endings.java
@@ -1,24 +1,31 @@
 public class Endings {
     public static void gameOverOne() {
         System.out.println("Without a look at the sides, you cross the Street. Screeching tires, a loud noise and a short pain on the right arm and head. Then NOTHING AS DARKNESS. Game over, a car with a poster of Big Brother on the side crashes into you.");
+        System.exit(0);
     }
     public static void gameOverTwo() {
         System.out.println("A loud BÄÄÄM at the door. then 5 people, all from the Thought Police, run into your flat and take position around you. A black bag over your face and a hard blow to the head. Then DARKNESS. (Game Over. The Thought Police come to get you)");
+        System.exit(0);
     }
     public static void gameOverThree() {
         System.out.println("You run after him, but he is very fast. Two meters behind him you both reach the street. A group of men stand next to the door. They hear the words the boy screams, come over and hold you. Unfortunately, they discover themselves as members of the Thought Police. They ask the boy for the reason of his screaming. He tells them what he had seen on your table. Immediately they put a black bag over your face and hit you hard on the head. You go black before the eyes.");
+        System.exit(0);
     }
     public static void gameOverFour() {
         System.out.println("Your leg hurts because of the varicose ulcer and the men behind you are athletic and very fast. After a short run through the corridors, they got you. They put a black bag over your face. You hear them say with laugh: “Another one for the gallows”.");
+        System.exit(0);
     }
     public static void gameOverFive() {
         System.out.println("One of them come closer and put a black bag over your face and hit you hard on the head. You go black before the eyes.");
+        System.exit(0);
     }
     public static void gameOverSix() {
         System.out.println("You pass the corner, then one more. Exactly when you run over the street a noisy sound about you, a light and a pain never felt. You lie on the street. Around you fearful faces. A child asks his mother why the man doesn´t have any legs. You feel a sticky wet liquid under your body. Then your view turns black. ");
+        System.exit(0);
     }
     public static void gameOverSeven() {
         System.out.println("You walk another street when a loud noisy sound comes from up to you. Then a loud explosion, a brightly light, a pain and darkness. Another bomb dropped directly in front of you. ");
+        System.exit(0);
     }
 
 }
