858fe6e61a39a58b70bfd5ce46580c9d39834c16
cash <info@cashmny.com>
2021-11-30 12:27:01 +0100

    new

---
+++ 1984/src/Endings.java
@@ -0,0 +1,187 @@
+public class Endings {
+	// Here are several ending texts
+    public static void gameOverOne() {
+        System.out.println("Without a look at the sides, you cross the Street. Screeching tires," 
+					+ "\na loud noise and a short pain on the right arm and head." 
+					+ "\n Then NOTHING AS DARKNESS. Game over, a car with a poster of Big Brother on the side crashes into you.");
+        System.exit(0);
+    }
+    public static void gameOverTwo() {
+        System.out.println("A loud BAAAAAM at the door. then 5 people, all from the Thought Police, " 
+					+ "\nrun into your flat and take position around you." 
+					+ "\nA black bag over your face and a hard blow to the head." 
+					+ "\nThen DARKNESS. (Game Over. The Thought Police come to get you)");
+        System.exit(0);
+    }
+    public static void gameOverThree() {
+        System.out.println("You run after him, but he is very fast." 
+					+ "\nTwo meters behind him you both reach the street." 
+					+ "\nA group of men stand next to the door. They hear the words the boy screams," 
+					+ "\ncome over and hold you. Unfortunately, they discover themselves as members of the Thought Police." 
+					+ "\nThey ask the boy for the reason of his screaming." 
+					+ "\nHe tells them what he had seen on your table." 
+					+ "\nImmediately they put a black bag over your face and hit you hard on the head. You go black before the eyes.");
+        System.exit(0);
+    }
+    public static void gameOverFour() {
+        System.out.println("Your leg hurts because of the varicose ulcer and the men behind you are athletic and very fast." 
+					+ "\nAfter a short run through the corridors, they got you." 
+					+ "\nThey put a black bag over your face." 
+					+ "\nYou hear them say with laugh: \"Another one for the gallows.\"");
+        System.exit(0);
+    }
+    public static void gameOverFive() {
+        System.out.println("One of them come closer and put a black bag over your face and hit you hard on the head." 
+					+ "\nYou go black before the eyes.");
+        System.exit(0);
+    }
+    public static void gameOverSix() {
+        System.out.println("You pass the corner, then one more." 
+					+ "\nExactly when you run over the street a noisy sound about you, a light and a pain never felt." 
+					+ "\nYou lie on the street. Around you fearful faces." 
+					+ "\nA child asks his mother why the man doesnÂ´t have any legs." 
+					+ "\nYou feel a sticky wet liquid under your body. Then your view turns black. ");
+        System.exit(0);
+    }
+    public static void gameOverSeven() {
+        System.out.println("You walk another street when a loud noisy sound comes from up to you." 
+					+ "\nThen a loud explosion, a brightly light, a pain and darkness." 
+					+ "\nAnother bomb dropped directly in front of you. ");
+        System.exit(0);
+    }
+    public static void gameOverEight() {
+        System.out.println("You try to leave London by train in a risky action during the night." 
+					+ "\nThe police are standing at the railroad track, your ID is checked." 
+					+ "\nYou are caught, the thought police arrest you.");
+        System.exit(0);
+    }
+    public static void gameOverNine() {
+        System.out.println("You sitting at home with beads of sweat on his forehead, they could show up here at any time." 
+					+ "\nYou try to burn your diary... there is a knock at the door... " 
+					+ "\nknock knock thoughts police, open the door, you are under arrest.");
+        System.exit(0);
+    }
+        public static void gameOverTen() {
+            System.out.println("As you run around a corner, you get hit on the head......" 
+					+ "\nYou wake up but you can't see, they put a bag over your head." 
+					+ "\nYou ask if someone can hear you." 
+					+ "\nBANG The Tought police didn't believe your excuse and chased you." 
+					+ "\nThey intercepted you, knocked you down and executed you.");
+            System.exit(0);        
+    }
+        public static void gameOverEleven() {
+            System.out.println("you hide behind a bush. Suddenly you feel a thud on your head." 
+					+ "\nYou wake up but you can't see, they put a bag over your head." 
+					+ "\nYou ask if someone can hear you." 
+					+ "\nBANG The Tought police didn't believe your excuse and chased you." 
+					+ "\nThey intercepted you, knocked you down and executed you.");
+            System.exit(0);
+    }
+        public static void gameOverTwelve() {
+            System.out.println("You go further down the corridor.\n"
+            		+ "….\n"
+            		+ "You hear footsteps behind you, or are they in front of you?\n"
+            		+ "….\n"
+            		+ "You speed up and your heart races. Suddenly the thought police appears in front of you. Your mistrust was justified, it is a trap.\n"
+            		+ "….\n"
+            		+ "The thought police grab you and drag you to the basement, when they drag you into a room O'brein s sitting in the room and smiling at you.\n"
+            		+ "….\n"
+            		+ "\"Gotcha Smith\"\n"
+            		+ "….\n"
+            		+ "You have been monitored by O'Brien all this time and now you are doomed to be vaporized.\n"
+            		+ "");
+            System.exit(0);
+    }
+        public static void gameOverThirteen() {
+            System.out.println("You go further down the corridor.\n"
+            		+ "….\n"
+            		+ "You hear footsteps behind you, or are they in front of you?\n"
+            		+ "….\n"
+            		+ "You speed up and your heart races. Suddenly the thought police appears in front of you. Your mistrust was justified, it is a trap.\n"
+            		+ "….\n"
+            		+ "The thought police grab you and drag you to the basement, when they drag you into a room O'Brien s sitting in the room and smiling at you.\n"
+            		+ "….\n"
+            		+ "\"Gotcha Smith\"\n"
+            		+ "….\n"
+            		+ "You have been monitored by O'Brien all this time and now you are doomed to be vaporized.\n"
+            		+ "");
+            System.exit(0);
+    }
+        public static void gameOverFourteen() {
+            System.out.println("You decide to go home. While Julia is sleeping, you secretly pack your things and leave.\n"
+            		+ "….\n"
+            		+ "as you go home you think about the things you have done. While you are walking down the street in your mind, you don't notice that police is following you. "
+            		+ "As you turn into an alley, a bag is pulled over your head and you feel a thud on your head.\n"
+            		+ "….\n"
+            		+ "Blood runs over your neck, you feel another hit.\n"
+            		+ "….\n"
+            		+ "With the last blow you hear a loud crack, and your eyes fall shut.\n"
+            		+ "");
+            System.exit(0);        
+    }
+        public static void gameOverFifteen() {
+            System.out.println("You start running as fast as you can across the roof first.\n"
+            		+ ".....\n"
+            		+ "After a few metres you step on a loose tile and slip.\n"
+            		+ "....\n"
+            		+ "you slide all the way down the roof and fall head over heels towards the ground.\n"
+            		+ "....\n"
+            		+ "you try to protect your head with your hands, but it is no use.\n"
+            		+ "....\n"
+            		+ "the last thing you hear is the crack of your skull.\n"
+            		+ "");
+            System.exit(0);        
+    }
+        public static void gameOverSixteen() {
+            System.out.println("You start to climb slowly over the roof ridge when the helicopter spots you.\n"
+            		+ "....\n"
+            		+ "you panic and start to \n"
+            		+ "run.\n"
+            		+ "....\n"
+            		+ "The wind from the helicopter knocks you off your feet and you slide down.\n"
+            		+ "....\n"
+            		+ "you slide all the way down the roof and fall head over heels towards the ground.\n"
+            		+ "....\n"
+            		+ "you try to protect your head with your hands, but it is no use.\n"
+            		+ "....\n"
+            		+ "the last thing you hear is the crack of your skull.\n"
+            		+ "");
+            System.exit(0);
+    }
+        public static void gameOverSeventeen() {
+            System.out.println("mhhh\n"
+            		+ "....\n"
+            		+ "unfortunately, this is not how it works Winston\n"
+            		+ "....\n"
+            		+ "All that work for nothing, you could have made it, but you leave me no choice.\n"
+            		+ "….\n"
+            		+ "With a swift movement, O'Brien places a wire noose around your neck.\n"
+            		+ "....\n"
+            		+ "That's it Winston.... that's it\n"
+            		+ "....\n"
+            		+ "O'Brien pulls harder and harder on the noose, you gasp for air, slowly you feel the wire cutting through the skin of your neck.\n"
+            		+ "....\n"
+            		+ "In one last gasp, you manage to say it one last time.\n"
+            		+ "....\n"
+            		+ "KILL BIG BROTHER\n");
+            System.exit(0);
+    }
+        public static void gameOverEightteen() {
+            System.out.println("poor Winston\n"
+            		+ "....\n"
+            		+ "I don't know how to make this end well\n"
+            		+ "....\n"
+            		+ "you leave me no choice\n"
+            		+ "….\n"
+            		+ "O'Brien nods to the guardian and leaves the room.\n"
+            		+ "....\n"
+            		+ "You only feel the cold steel of the gun barrel resting on the back of your head.\n"
+            		+ "....\n"
+            		+ "finally, it's over, you say with a slight smile, before you hear the click of the trigger.\n");
+            System.exit(0);
+    }
+        public static void gameOverNineteen() {
+            System.out.println("You have been exposed as a traitor. The gallow await for you.");
+            System.exit(0);
+        }
+}
